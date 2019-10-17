4de85b8fe48cfa0ee1fbaf439d3abdb8e9332c4f
HrithikMittal <adhikanshmittal@gmail.com>
2019-10-17 08:37:56 +0530

    how to start column added

--- js/script.js
+++ js/script.js
@@ -56,3 +56,13 @@ window.onload = function() {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
   document.body.appendChild(css);
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, options);
+});
+// Or with jQuery
+
+$(document).ready(function() {
+  $(".collapsible").collapsible();
+});
